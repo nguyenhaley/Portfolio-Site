db6ffd87d6e43bbb43036e058885c1f75fe3a704
nguyenhaley <hhn007@ucsd.edu>
2025-08-12 09:10:17 -0700

    made media query changes

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  // hamburger menu logic
+  var icon = document.getElementById("icon");
+  var icon1 = document.getElementById("a");
+  var icon2 = document.getElementById("b");
+  var icon3 = document.getElementById("c");
+  var navbar = document.getElementById("navbar");
 
-    // hamburger menu logic
-    var icon = document.getElementById("icon");
-    var icon1 = document.getElementById("a");
-    var icon2 = document.getElementById("b");
-    var icon3 = document.getElementById("c");
-    var navbar = document.getElementById('navbar');
-
-    icon.addEventListener('click', function() {
-        icon1.classList.toggle('a');
-        icon2.classList.toggle('c');
-        icon3.classList.toggle('b');
-        navbar.classList.toggle('show');
-    });
+  icon.addEventListener("click", function () {
+    icon1.classList.toggle("a");
+    icon2.classList.toggle("c");
+    icon3.classList.toggle("b");
+    navbar.classList.toggle("show");
+  });
 });
 
 // fullpage js logic
 $(document).ready(function () {
-    $('#fullpage').fullpage({
-        anchors: ['welcome-page', 'main', 'about', 'contact'],
-        navigation: true,
-        navigationPosition: 'right',
-        navigationToolTips: ['Welcome', 'Intro', 'About Me', 'Contact Me'],
-        showActiveTooltip: true,
-        scrollOverflow: true
-    });
+  $("#fullpage").fullpage({
+    anchors: ["welcome-page", "main", "about", "contact"],
+    navigation: true,
+    navigationPosition: "right",
+    navigationToolTips: ["Welcome", "Intro", "About Me", "Contact Me"],
+    showActiveTooltip: true,
+    scrollOverflow: true,
+  });
 });
